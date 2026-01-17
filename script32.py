431e28d107c483ae71e7bf83e85e8e3dff099273
kascrizzi <110206911+kascrizzi@users.noreply.github.com>
2026-01-17 11:24:13 -0500

    Add files via upload

---
+++ script32.py
@@ -0,0 +1,35 @@
+### m03f/script32.py
+my_book = input('What book would you like as a script? ')
+
+with open('txts/' + my_book) as my_open_book:
+    # Start state
+    looking_for_open_quote = True
+
+    while True:
+        the_line = my_open_book.readline()
+
+        # Check for EOF
+        if the_line == '':
+            break
+
+        # new pseudocode goes here
+        if looking_for_open_quote:   # in S0
+            i = the_line.find('"')
+            if i != -1:  # Was the find successful?
+                # Found an open quote
+                dialog = the_line[i:]
+                looking_for_open_quote = False
+            # FIXME! Is this all the work in state S0?
+
+        else:                        # in S1
+            i = the_line.find('"')
+            if i != -1:  # Was the find successful?
+                # Found a close quote
+                dialog += the_line[:i+1]
+                print("\nACTOR:", dialog)
+                looking_for_open_quote = True
+            else:
+                dialog += the_line
+            # FIXME! Is this all the work in state S1?
+
+print("The End.")
